7674e1b915a87163aa693cf79b3a7b896deb915f
kingawb <kwigurskabak+gh@gmail.com>
2020-06-17 05:31:26 +0200

    Init

---
+++ scripts/class-name.js
@@ -0,0 +1,9 @@
+// Class file
+  export class ClassName {
+    constructor(settings) {
+      this.config = {};
+
+      Object.assign(this.config, settings);
+    }
+
+  }
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const { ClassName } = require('./class-name');
+
+// Main file where you execute function from class-name.js
+export { ClassName } from './class-name.js';
+
+function main() {
+  // querySelectors
+
+  // class usage
+}
+
+window.addEventListener('load', main);
