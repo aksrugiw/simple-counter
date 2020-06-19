c8512b6f20008c3ccea329f13c23f97b207615cd
aksrugiw <kwigurskabak@gmail.com>
2020-06-20 00:01:49 +0200

    Update main tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,20 +1,33 @@
 import { Counter } from '../scripts/counter.js';
 import { Notification } from '../scripts/notification.js';
 
-let count, current, increaseButton, decreaseButton, counter;
-beforeEach(() => {
+let count, current, increaseButton, decreaseButton, counter, close, overlay, notificationContent, notification;
+beforeAll(() => {
   document.body.innerHTML = `
   <h2>Count: <span class="count">2</span></h2>
   <button class="increase-btn">+</button>
   <button class="decrease-btn">-</button>
+  <div class="overlay hide">
+    <div class="notification">
+      <p class="content"></p>
+      <button class="close"></button>
+    </div>
+  </div>
 `;
   count = document.querySelector('.count');
   current = parseInt(count.innerHTML);
   increaseButton = document.querySelector('.increase-btn');
   decreaseButton = document.querySelector('.decrease-btn');
-  const notification = new Notification({
+
+  close = document.querySelector('.notification .close');
+  overlay = document.querySelector('.overlay');
+  notificationContent = document.querySelector('.notification .content');
+  notification = new Notification({
     showNotification: jest.fn(),
-    messages: { 1: 'test' },
+    messages: { 3: 'test notification' },
+    close,
+    overlay,
+    notificationContent,
   });
   counter = new Counter({
     current,
@@ -25,7 +38,7 @@ beforeEach(() => {
   });
 });
 
-describe('main', () => {
+describe('main counter', () => {
   test('update current number in element after increase', () => {
     counter.setupClickEvents();
 
@@ -39,5 +52,21 @@ describe('main', () => {
     decreaseButton.click();
 
     expect(parseInt(count.innerHTML)).toBe(1);
+  });
+});
+describe('main notification', () => {
+  test('display overlay after increase when reach specific number', () => {
+    counter.setupClickEvents();
+
+    increaseButton.click();
+
+    expect(overlay.classList.contains('hide')).toBe(false);
+  });
+  test('display notification with specific messages after increase when reach specific number', () => {
+    counter.setupClickEvents();
+
+    increaseButton.click();
+
+    expect(notificationContent.innerHTML).toBe('test notification');
   });
 });
