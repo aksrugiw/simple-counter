82476bdefd36b9d83c20e2a3e8130ed0a8cde99f
aksrugiw <kwigurskabak@gmail.com>
2020-06-19 15:59:16 +0200

    Show message after click

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,3 +1,14 @@
+const MESSAGE = {
+  0: 'No, no, only positive numbers 🤓',
+  5: 'That’s a good start, keep going 👌',
+  13: 'Oh no, unlucky 13! 😱 Quick! Change it!',
+  20: 'Wow, you’re so patient 😅',
+  50: 'Still there? Are you bored or sth? 😴',
+  100: 'Nice job! 👍 You got 💯 points!',
+  110: 'Really? Still there? Nothing will happen anymore... 😐',
+  150: 'Come on! Go home, you’re drunk 🍻😃',
+};
+
 export class Counter {
   constructor(settings) {
     this.config = {};
@@ -21,11 +32,29 @@ export class Counter {
   render() {
     if (this.config.count) {
       this.config.count.innerHTML = this.config.current;
+      this.showMessage();
     }
+  }
+
+  showMessage() {
+    if (Object.keys(MESSAGE).includes(this.config.current.toString())) {
+      return this.showNotification(MESSAGE[this.config.current]);
+    }
+  }
+
+  hideNotification() {
+    this.config.overlay.classList.add('hide');
+  }
+
+  showNotification(message) {
+    this.config.notificationContent.innerHTML = message;
+    this.config.overlay.classList.remove('hide');
   }
 
   setupClickEvents() {
     this.config.increaseButton.addEventListener('click', () => this.increase());
     this.config.decreaseButton.addEventListener('click', () => this.decrease());
+    this.config.close.addEventListener('click', () => this.hideNotification());
+    this.config.overlay.addEventListener('click', () => this.hideNotification());
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,11 @@ function main() {
   const current = parseInt(count.innerHTML);
   const increaseButton = document.querySelector('.increase-btn');
   const decreaseButton = document.querySelector('.decrease-btn');
+  const close = document.querySelector('.notification .close');
+  const overlay = document.querySelector('.overlay');
+  const notificationContent = document.querySelector('.notification .content');
 
-  const counter = new Counter({ current, count, increaseButton, decreaseButton });
+  const counter = new Counter({ current, count, increaseButton, decreaseButton, close, overlay, notificationContent });
   counter.setupClickEvents();
 }
 
