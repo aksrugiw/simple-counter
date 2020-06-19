49594e896f1c643c5eb250789b3d7979630b37df
aksrugiw <kwigurskabak@gmail.com>
2020-06-20 00:59:33 +0200

    Add tests for notification module

---
+++ test/notification.test.js
@@ -0,0 +1,39 @@
+import { Notification } from '../scripts/notification.js';
+
+beforeAll(() => {
+  document.body.innerHTML = `
+  <h2>Count: <span class="count">2</span></h2>
+  <button class="increase-btn">+</button>
+  <button class="decrease-btn">-</button>
+  <div class="overlay">
+    <div class="notification">
+      <p class="content"></p>
+      <button class="close"></button>
+    </div>
+  </div>
+`;
+});
+describe('Notification', () => {
+  test('create object', () => {
+    const notification = new Notification({
+      messages: { 3: 'test notification' },
+    });
+
+    expect(notification.config.messages).toEqual({ 3: 'test notification' });
+  });
+
+  test('hide notification after click close button', () => {
+    const close = document.querySelector('.notification .close');
+    const overlay = document.querySelector('.overlay');
+    const notification = new Notification({
+      close,
+      overlay,
+    });
+    notification.hideNotification = jest.fn();
+
+    notification.setupClickEvents();
+    close.click();
+
+    expect(notification.hideNotification).toHaveBeenCalled();
+  });
+});
